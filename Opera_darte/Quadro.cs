ccd5bc420b6c8645f579983e36932cac13f032a2
ghilardi.19320 <ghilardi.19320@LABI1-6PC12>
2023-01-09 08:35:37 +0100

    Aggiungere i file di progetto.

---
+++ Opera_darte/Quadro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opera_darte
+{
+    public class Quadro
+    {
+
+        private string _id, _altezza, _lung, _artista;
+        private float _prezzoOff, _prezzoTras;
+        bool _richiediTras = false;
+
+
+
+
+    }
+}
